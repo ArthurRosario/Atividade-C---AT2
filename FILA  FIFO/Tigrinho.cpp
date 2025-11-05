468f5de7b006e5c3a957a4a9f0f2e8bda511cb68
PauloAthur <arthurking.pereira@gmail.com>
2025-11-05 20:16:58 -0300

    a

--- FILA  FIFO/Tigrinho.cpp
+++ FILA  FIFO/Tigrinho.cpp
@@ -10,6 +10,18 @@ int main(){
     //criação da fila
     queue<string> fila_sorteio;
     
-    cout << "Sistema de sorteio";
-    
+    cout << "Sistema de sorteio\n\n";
+    // Verificar se a fila está vazia
+    cout << fila_sorteio.empty();
+    cout << "inscrição de times\n\n";
+
+    fila_sorteio.push("Flamengo\n"); //Primeiro a entrar
+    cout << "Flamengo entrou\n";
+
+    fila_sorteio.push("palmeiras\n"); //Segundo a entrar
+    cout << "Palmeiras entrou\n";
+
+    cout << "Tamanho da fila: "<< fila_sorteio.size(); // Tamanho da fila
+
+    return 0; // termina o programa
 }
